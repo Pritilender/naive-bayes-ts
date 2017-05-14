059111d9ece236f0057875ddba74a78879e3570d
Pritilender <sajn_ap@live.com>
2017-05-14 22:53:45 +0200

    fix bad classification

--- index.ts
+++ index.ts
@@ -1,21 +1,46 @@
 import {NaiveBayesClassifier} from './models/naive-bayes'
-import * as fs from 'fs'
-import * as readline from 'readline'
+const fs = require('fs')
+const readline = require('readline')
 
-const trainClassifier = () => {
-  const trainDataFolder: string = `${__dirname}/data/train/`
-  fs.readdirSync(trainDataFolder).forEach(file => {
-    let data: string[] = fs.readFileSync(trainDataFolder + file).toString('utf-8').split('\n')
+interface Stats {
+  hits: number
+  miss: number
+  total: number
+}
 
-    data.forEach(doc => {
-      if (doc != ``) {
-        let firstSpace = doc.indexOf('\t')
-        let label = doc.substring(0, firstSpace)
-        let text = doc.slice(firstSpace + 1)
-        bayes.train(text, label)
-      }
-    })
+const readData = (funct: Function, folderPath: string) => {
+  fs.readdirSync(folderPath).forEach(path => {
+    const fullPath = `${folderPath}/${path}`
+    if (fs.statSync(fullPath).isDirectory()) {
+      readData(funct, fullPath)
+    } else if (fs.statSync(fullPath).isFile()) {
+      fs.readFileSync(fullPath).toString('utf-8').split('\n').forEach(doc => funct(doc))
+    }
   })
+}
+
+const trainClassifier = (doc) => {
+  if (doc != ``) {
+    let firstSpace = doc.indexOf('\t')
+    let label = doc.substring(0, firstSpace)
+    let text = doc.slice(firstSpace + 1)
+    bayes.train(text, label)
+  }
+}
+
+const testClassifier = (stats: Stats) => (doc) => {
+  if (doc != ``) {
+    const firstSpace = doc.indexOf('\t')
+    const label = doc.substring(0, firstSpace)
+    const text = doc.slice(firstSpace + 1)
+    const guessedLabel = bayes.classify(text)
+    if (label == guessedLabel) {
+      stats.hits++
+    } else {
+      stats.miss++
+    }
+    stats.total++
+  }
 }
 
 const args: Set<string> = new Set(process.argv.slice(2))
@@ -28,7 +53,8 @@ Written by Mihajlo Ilijic.
 Usage:
 '--train' or '-t' - read train corpus and train the classifier
 '--write-out' or '-w' - save classifier to './export' folder
-'--help' or '-h' - writes this help message`)
+'--help' or '-h' - writes this help message
+'--stats' - writes statistics based on training and testing data`)
 } else {
   const lineReader = readline.createInterface(<any>{
     input: process.stdin,
@@ -38,7 +64,7 @@ Usage:
 
   if (args.has('--train') || args.has('-t')) {
     console.log('Training classifier...')
-    trainClassifier()
+    readData(trainClassifier, `${__dirname}/data/reuteres-train`)
     console.log('Done!')
   }
 
@@ -48,14 +74,28 @@ Usage:
     console.log(`Done!`)
   }
 
-  lineReader.prompt()
-  lineReader.on('line', (line) => {
-    if (line == 'q') {
-      lineReader.close()
-    } else {
-      const docClass = bayes.classify(line)
-      console.log(`Document class is ${docClass}.`)
-      lineReader.prompt()
+  if (args.has('--stats')) {
+    const stats: Stats = {
+      hits: 0,
+      miss: 0,
+      total: 0,
     }
-  })
+    readData(testClassifier(stats), `${__dirname}/data/reuteres-test`)
+    console.log(`Statistics:
+* total docs: ${stats.total}
+* hit count: ${stats.hits} (${((stats.hits / stats.total) * 100).toFixed(2)}%)
+* miss count: ${stats.miss} (${((stats.miss / stats.total) * 100).toFixed(2)}%)`)
+    lineReader.close()
+  } else {
+    lineReader.prompt()
+    lineReader.on('line', (line) => {
+      if (line == 'q') {
+        lineReader.close()
+      } else {
+        const docClass = bayes.classify(line)
+        console.log(`Document class is ${docClass}.`)
+        lineReader.prompt()
+      }
+    })
+  }
 }
--- models/naive-bayes.ts
+++ models/naive-bayes.ts
@@ -1,119 +1,117 @@
-import * as fs from 'fs'
-interface TokenOccurrence {
+const fs = require('fs')
+const stemmer = require('stemmer')
+
+interface DocumentLabelInfo {
   label: string
-  docCount: number
-  tokenCount: number
-  tokenInfo: {
-    token: string;
+  documentCount: number
+  wordCount: number
+  wordInfo: {
+    word: string;
     occurrence: number;
   }[]
 }
 
 export class NaiveBayesClassifier {
-  private _tokenOccurrenceObject: TokenOccurrence[]
-  private _documentCount: number
+  private _labelsAndInfo: DocumentLabelInfo[]
+  private _totalDocumentCount: number
+  private _vocabulary: Set<string>
 
   private constructor() {
-    this._tokenOccurrenceObject = []
-    this._documentCount = 0
-  }
-
-  public train(document: string, label: string) {
-    const words = this.extractWordsFromDocument(document)
-    let labelIndex: number = this._tokenOccurrenceObject.findIndex(el => el.label == label)
-
-    this._documentCount++
-
-    if (labelIndex > -1) {
-      this._tokenOccurrenceObject[labelIndex].docCount++
-    } else {
-      this._tokenOccurrenceObject.push({
-        label,
-        docCount: 1,
-        tokenInfo: [],
-        tokenCount: 0,
-      })
-      labelIndex = this._tokenOccurrenceObject.length - 1
-    }
-
-    words.forEach(word => {
-      const wordIndex = this._tokenOccurrenceObject[labelIndex].tokenInfo.findIndex(tokenInfo => tokenInfo.token == word)
-      if (wordIndex > -1) {
-        this._tokenOccurrenceObject[labelIndex].tokenInfo[wordIndex].occurrence++
-      } else {
-        this._tokenOccurrenceObject[labelIndex].tokenInfo.push({token: word, occurrence: 1})
-      }
-      this._tokenOccurrenceObject[labelIndex].tokenCount++
-    })
-  }
-
-  public toString() {
-    return JSON.stringify(this)
+    this._labelsAndInfo = []
+    this._totalDocumentCount = 0
+    this._vocabulary = new Set()
   }
 
   public static create(path: string): NaiveBayesClassifier {
     const instance: NaiveBayesClassifier = new NaiveBayesClassifier
     if (fs.existsSync(path)) {
       const properties: NaiveBayesClassifier = JSON.parse(fs.readFileSync(path, 'utf-8').toString())
-      instance._documentCount = properties._documentCount
-      instance._tokenOccurrenceObject = properties._tokenOccurrenceObject
+      instance._totalDocumentCount = properties._totalDocumentCount
+      instance._labelsAndInfo = properties._labelsAndInfo
     }
     return instance
   }
 
   public toFile(path: string) {
-    fs.writeFileSync(path, this, 'utf-8')
+    fs.writeFileSync(path, JSON.stringify(this), 'utf-8')
+  }
+
+  public train(document: string, label: string) {
+    const words = this.extractWordsFromDocument(document)
+    const labelIndex = this.addLabel(label)
+
+    this._totalDocumentCount++
+    this._labelsAndInfo[labelIndex].documentCount++
+
+    words.forEach(word => {
+      const wordIndex: number = this.addWordInLabel(word, labelIndex)
+      this._labelsAndInfo[labelIndex].wordInfo[wordIndex].occurrence++
+      this._labelsAndInfo[labelIndex].wordCount++
+    })
+  }
+
+  private addWordInLabel(word: string, labelIndex: number): number {
+    let wordIndex = this._labelsAndInfo[labelIndex]
+      .wordInfo
+      .findIndex(tokenInfo => tokenInfo.word == word)
+
+    if (wordIndex == -1) {
+      this._labelsAndInfo[labelIndex]
+        .wordInfo
+        .push({word: word, occurrence: 0})
+      wordIndex = this._labelsAndInfo[labelIndex].wordInfo.length - 1
+      this._vocabulary.add(word)
+    }
+
+    return wordIndex
+  }
+
+  private addLabel(label: string) {
+    let labelIndex: number = this._labelsAndInfo.findIndex(el => el.label == label)
+
+    if (labelIndex == -1) {
+      this._labelsAndInfo.push({
+        label,
+        documentCount: 0,
+        wordInfo: [],
+        wordCount: 0,
+      })
+      labelIndex = this._labelsAndInfo.length - 1
+    }
+
+    return labelIndex
   }
 
   public classify(document: string): string {
     const words = this.extractWordsFromDocument(document)
-    const sumOfTokens: any = {}
-    let probabilities: any = {}
 
-    this._tokenOccurrenceObject.forEach(el => sumOfTokens[el.label] = this.allTokenOccurrence(el.label))
-    this._tokenOccurrenceObject.forEach(el => probabilities[el.label] = 0)
-
-    this._tokenOccurrenceObject.forEach(tokenOccurrence => {
-      const label = tokenOccurrence.label
-      const tokens = []
-      tokenOccurrence.tokenInfo.forEach(tokenInfo => tokens[tokenInfo.token] = tokenInfo.occurrence)
+    let maxProbability = -Infinity
+    let maxLabel = ''
+    this._labelsAndInfo.forEach(labelAndInfo => {
+      let probability: number = Math.log(labelAndInfo.documentCount / this._totalDocumentCount)
+      const wordCount = labelAndInfo.wordCount
 
       words.forEach(word => {
-        const prob = tokenOccurrence[word] ? (tokenOccurrence[word] + 1) : 1
-        probabilities[label] += Math.log(prob) - Math.log(sumOfTokens[label] + tokenOccurrence.tokenCount)
+        const wordInClass = labelAndInfo.wordInfo.find(ti => ti.word == word)
+        const wordFrequencyInClass = wordInClass ? wordInClass.occurrence : 0
+        const wordProbability = (wordFrequencyInClass + 1) / (wordCount + this._vocabulary.size)
+
+        probability += Math.log(wordProbability)
       })
 
-      probabilities[label] += Math.log(tokenOccurrence.docCount) - Math.log(this._documentCount)
+      if (probability > maxProbability) {
+        maxProbability = probability
+        maxLabel = labelAndInfo.label
+      }
     })
 
-    console.log(probabilities)
-
-    return this.maxProbability(probabilities)
-  }
-
-  private maxProbability(probabilities: any): string {
-    let maxLabel = ''
-    let maxVal = 0
-    for (let key in probabilities) {
-      if (probabilities.hasOwnProperty(key)) {
-        if (Math.abs(probabilities[key]) > maxVal) {
-          maxVal = Math.abs(probabilities[key])
-          maxLabel = key
-        }
-      }
-    }
-
     return maxLabel
-  }
-
-  private allTokenOccurrence(label: string): number {
-    return this._tokenOccurrenceObject.find(el => el.label == label).tokenCount
   }
 
   private extractWordsFromDocument(document: string): string[] {
     let words: string[] = document.toLowerCase()
       .split(' ')
-    words = words.map(word => word.replace(/\W/g, '').trim())
+    words = words.map(word => stemmer(word.replace(/\W/g, '').trim()))
     return words
   }
 }

fa27599783a29f29b18c3825100e6bde058c30eb
Pritilender <sajn_ap@live.com>
2017-05-19 02:26:40 +0200

    add classifier options

--- index.ts
+++ index.ts
@@ -8,32 +8,45 @@ interface Stats {
   total: number
 }
 
-const readData = (funct: Function, folderPath: string) => {
+const readData = (funct: Function, folderPath: string, top: number = 25000) => {
+  let i = 0
   fs.readdirSync(folderPath).forEach(path => {
     const fullPath = `${folderPath}/${path}`
     if (fs.statSync(fullPath).isDirectory()) {
       readData(funct, fullPath)
     } else if (fs.statSync(fullPath).isFile()) {
-      fs.readFileSync(fullPath).toString('utf-8').split('\n').forEach(doc => funct(doc))
+      if (i < top) {
+        fs.readFileSync(fullPath).toString('utf-8').split('\n').forEach(doc => funct(doc))
+        i++
+      }
     }
   })
 }
 
-const trainClassifier = (doc) => {
+const trainClassifier = (classifier: NaiveBayesClassifier) => (doc: string) => {
   if (doc != ``) {
     let firstSpace = doc.indexOf('\t')
     let label = doc.substring(0, firstSpace)
     let text = doc.slice(firstSpace + 1)
-    bayes.train(text, label)
+    classifier.train(text, label)
   }
 }
 
-const testClassifier = (stats: Stats) => (doc) => {
+const trainClassifiers = (classifiers: NaiveBayesClassifier[]) => (doc: string) => {
+  if (doc != ``) {
+    let firstSpace = doc.indexOf('\t')
+    let label = doc.substring(0, firstSpace)
+    let text = doc.slice(firstSpace + 1)
+    classifiers.forEach(classifier => classifier.train(text, label))
+  }
+}
+
+const testClassifier = (classifier: NaiveBayesClassifier, stats: Stats) => (doc: string) => {
   if (doc != ``) {
     const firstSpace = doc.indexOf('\t')
     const label = doc.substring(0, firstSpace)
     const text = doc.slice(firstSpace + 1)
-    const guessedLabel = bayes.classify(text)
+    const guessedLabel = classifier.classify(text)
     if (label == guessedLabel) {
       stats.hits++
     } else {
@@ -43,9 +56,47 @@ const testClassifier = (stats: Stats) => (doc) => {
   }
 }
 
+const stats = (classifier: NaiveBayesClassifier, path: string) => {
+  const stats: Stats = {
+    hits: 0,
+    miss: 0,
+    total: 0,
+  }
+  readData(testClassifier(classifier, stats), `${__dirname}/data/test`)
+  console.log(`Statistics for ${path}:
+* total docs: ${stats.total}
+* hit count: ${stats.hits} (${((stats.hits / stats.total) * 100).toFixed(2)}%)
+* miss count: ${stats.miss} (${((stats.miss / stats.total) * 100).toFixed(2)}%)`)
+}
+
 const args: Set<string> = new Set(process.argv.slice(2))
-const pathToClassifier = `${__dirname}/export/bayes.json`
-const bayes = NaiveBayesClassifier.create(pathToClassifier)
+
+const bayesClassifiers: { classifier: NaiveBayesClassifier, path: string }[] = [
+  {
+    classifier: new NaiveBayesClassifier(),
+    path: `${__dirname}/export/u-f.json`,
+  },
+  {
+    classifier: new NaiveBayesClassifier({presence: true}),
+    path: `${__dirname}/export/u-p.json`,
+  },
+  {
+    classifier: new NaiveBayesClassifier({useNegative: true}),
+    path: `${__dirname}/export/u-n-f.json`,
+  },
+  {
+    classifier: new NaiveBayesClassifier({presence: true, useNegative: true}),
+    path: `${__dirname}/export/u-n-p.json`,
+  },
+  {
+    classifier: new NaiveBayesClassifier({bigrams: true}),
+    path: `${__dirname}/export/b-f.json`,
+  },
+  {
+    classifier: new NaiveBayesClassifier({bigrams: true, presence: true}),
+    path: `${__dirname}/export/b-p.json`
+  },
+]
 
 if (args.has('--help') || args.has('-h')) {
   console.log(`Naive Bayes classifier NodeJS command-line executable.
@@ -54,7 +105,21 @@ Usage:
 '--train' or '-t' - read train corpus and train the classifier
 '--write-out' or '-w' - save classifier to './export' folder
 '--help' or '-h' - writes this help message
-'--stats' - writes statistics based on training and testing data`)
+'--stats' - writes statistics based on training and testing data
+'--fix-data' - reformat test and train data`)
+} else if (args.has('--fix-data')) {
+  fs.readdirSync('./data').forEach(subDir => {
+    fs.readdirSync(`./data/${subDir}`).forEach(tagDir => {
+      let data: string = ''
+      let noRead = 0
+      fs.readdirSync(`./data/${subDir}/${tagDir}`).forEach(file => {
+        data += fs.readFileSync(`./data/${subDir}/${tagDir}/${file}`).toString('utf-8') + '\n'
+        noRead++
+      })
+      fs.writeFileSync(`./data/${subDir}/${tagDir}/merged.txt`, data)
+      console.log(`written to ./data/${subDir}/${tagDir} and count is ${noRead}`)
+    })
+  })
 } else {
   const lineReader = readline.createInterface(<any>{
     input: process.stdin,
@@ -64,27 +129,32 @@ Usage:
 
   if (args.has('--train') || args.has('-t')) {
     console.log('Training classifier...')
-    readData(trainClassifier, `${__dirname}/data/reuteres-train`)
+
+    readData(trainClassifiers(bayesClassifiers.map(el => el.classifier)), `./data/train`, 1000)
+
     console.log('Done!')
   }
 
   if (args.has('--write-out') || args.has('-w')) {
-    console.log(`Writing classifier to ${pathToClassifier}`)
-    bayes.toFile(pathToClassifier)
+
+    // WRITE OUT
+    bayesClassifiers.forEach(el => {
+      console.log('writing out', el.path)
+      el.classifier.toFile(el.path)
+      console.log('done')
+    })
+
     console.log(`Done!`)
   }
 
   if (args.has('--stats')) {
-    const stats: Stats = {
-      hits: 0,
-      miss: 0,
-      total: 0,
-    }
-    readData(testClassifier(stats), `${__dirname}/data/reuteres-test`)
-    console.log(`Statistics:
-* total docs: ${stats.total}
-* hit count: ${stats.hits} (${((stats.hits / stats.total) * 100).toFixed(2)}%)
-* miss count: ${stats.miss} (${((stats.miss / stats.total) * 100).toFixed(2)}%)`)
+
+    // STATS
+    bayesClassifiers.forEach(el => {
+      console.log('statistics...', el.path)
+      stats(el.classifier, el.path)
+    })
+
     lineReader.close()
   } else {
     lineReader.prompt()
@@ -92,8 +162,9 @@ Usage:
       if (line == 'q') {
         lineReader.close()
       } else {
-        const docClass = bayes.classify(line)
-        console.log(`Document class is ${docClass}.`)
+        // TODO
+        // const docClass = unigramFreqBayes.classify(line)
+        // console.log(`Document class is ${docClass}.`)
         lineReader.prompt()
       }
     })
--- naive-bayes.ts
+++ naive-bayes.ts
@@ -11,29 +11,48 @@ interface DocumentLabelInfo {
   }[]
 }
 
+interface ClassifierOptions {
+  presence?: boolean
+  bigrams?: boolean
+  useNegative?: boolean // only if bigrams is false
+}
+
 export class NaiveBayesClassifier {
   private _labelsAndInfo: DocumentLabelInfo[]
   private _totalDocumentCount: number
   private _vocabulary: Set<string>
+  private _presence: boolean
+  private _bigrams: boolean
+  private _useNegative: boolean
 
-  private constructor() {
+  public constructor(options?: ClassifierOptions) {
+    const opt = options || {}
+    const presence = opt.presence || false
+    const bigrams = opt.bigrams || false
+    const useNegative = (!bigrams && opt.bigrams) || false
+
     this._labelsAndInfo = []
     this._totalDocumentCount = 0
     this._vocabulary = new Set()
-  }
-
-  public static create(path: string): NaiveBayesClassifier {
-    const instance: NaiveBayesClassifier = new NaiveBayesClassifier
-    if (fs.existsSync(path)) {
-      const properties: NaiveBayesClassifier = JSON.parse(fs.readFileSync(path, 'utf-8').toString())
-      instance._totalDocumentCount = properties._totalDocumentCount
-      instance._labelsAndInfo = properties._labelsAndInfo
-    }
-    return instance
+    this._presence = presence
+    this._bigrams = bigrams
+    this._useNegative = useNegative
   }
 
   public toFile(path: string) {
     fs.writeFileSync(path, JSON.stringify(this), 'utf-8')
+  }
+
+  public parseFile(path: string) {
+    if (fs.existsSync(path)) {
+      const properties: NaiveBayesClassifier = JSON.parse(fs.readFileSync(path, 'utf-8').toString())
+      this._totalDocumentCount = properties._totalDocumentCount
+      this._labelsAndInfo = properties._labelsAndInfo
+      this._labelsAndInfo.forEach(doc => doc.wordInfo.forEach(wordInfo => this._vocabulary.add(wordInfo.word)))
+      this._presence = properties._presence
+      this._bigrams = properties._bigrams
+      this._useNegative = properties._useNegative
+    }
   }
 
   public train(document: string, label: string) {
@@ -42,6 +61,10 @@ export class NaiveBayesClassifier {
 
     this._totalDocumentCount++
     this._labelsAndInfo[labelIndex].documentCount++
+
+    if (!(this._totalDocumentCount % 1000)) {
+      console.log('total docs', this._totalDocumentCount)
+    }
 
     words.forEach(word => {
       const wordIndex: number = this.addWordInLabel(word, labelIndex)
@@ -111,7 +134,37 @@ export class NaiveBayesClassifier {
   private extractWordsFromDocument(document: string): string[] {
     let words: string[] = document.toLowerCase()
       .split(' ')
-    words = words.map(word => stemmer(word.replace(/\W/g, '').trim()))
+    // words = words.map(word => stemmer(word.replace(/\W/g, '').trim())
+    words = words.map(word => word.replace(/\W/g, '').trim())
+
+    if (this._bigrams) {
+      let bigramWords = []
+
+      for (let i = 0; i < words.length; i += 2) {
+        bigramWords.push(`${words[i]} ${words[i+1]}`)
+      }
+
+      if (!(words.length % 2)) {
+        bigramWords.push(words[words.length - 1])
+      }
+
+      words = bigramWords
+    } else if (this._useNegative) {
+      words = words.map((word, i) => {
+        const prev = words[i-1]
+        if (prev) {
+          word = prev.endsWith(`nt`) || prev.endsWith('not') ? `NOT_${word}` : word
+        }
+        return word
+      })
+    }
+
+    if (this._presence) {
+      const helperSet = new Set(words)
+      words = [...helperSet]
+    }
+
     return words
   }
+
 }
